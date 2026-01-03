6ca76b8ae8893b0fb44c0551be83cdcc5a9be1af
Pankaj Jain <jainm22570@gmail.com>
2026-01-03 14:21:48 +0530

    manage users functionality for editors

--- blogs/models.py
+++ blogs/models.py
@@ -1,61 +1,70 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Category(models.Model):
-    Category_name = models.CharField(max_length=50, unique = True)
-    created_at = models.DateTimeField(auto_now_add= True)
+    Category_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         verbose_name_plural = 'categories'
-        
+
     def __str__(self):
         return self.Category_name
 
+
 STATUS_CHOICES = (
     ("draft", "Draft"),
-    ("published","Published")
+    ("published", "Published")
 )
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    slug =models.SlugField(max_length=150 , unique = True , blank = True)
-    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
     short_description = models.TextField(max_length=500)
-    blog_body =models.TextField(max_length=2000)
-    status = models.CharField(max_length=20 ,choices=STATUS_CHOICES, default = "draft")
+    blog_body = models.TextField(max_length=2000)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="draft")
     is_featured = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add= True)
-    updated_at = models.DateTimeField(auto_now= True)
-    
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
+
 class About(models.Model):
     about = models.TextField(default="About coming soon")
-    about_description = models.TextField(max_length=2555,default="no description allowed")
+    about_description = models.TextField(max_length=2555, default="no description allowed")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         verbose_name_plural = 'About'
-        
+
     def __str__(self):
         return self.about
-    
+
+
 class SocialLinks(models.Model):
     platform = models.CharField(max_length=25)
     link = models.URLField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-        
-    
+
     class Meta:
         verbose_name_plural = 'Social Links'
-    
+
     def __str__(self):
         return self.platform
